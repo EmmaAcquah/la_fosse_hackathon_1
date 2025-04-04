d5ac1169123792794fc7c0964cac358a2ea5562a
MrDanielHo <daniel.kun.tin.ho@gmail.com>
2025-04-04 10:57:38 +0100

    Renaming the diceRoll function to be inline with the folder and filename.

--- DiceRoll/diceRoll.js
+++ DiceRoll/diceRoll.js
@@ -1,6 +1,10 @@
 
-function rollDice(sides) {
+function diceRoll(sides) {
     return Math.floor(Math.random()*sides)
 }
 
-console.log(rollDice(6));
+// console.log(diceRoll(6));
+
+module.exports = {
+    diceRoll
+}
